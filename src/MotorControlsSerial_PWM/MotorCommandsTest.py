bba4d5c110a2df2ebe18008d1a98cf3ffa753119
GeorgeTMartin <georgetnhmartin@gmail.com>
2023-01-20 15:48:32 -0800

    adding RPLIDAR Launch, Setting Correct tf dimensions

--- src/MotorControlsSerial_PWM/MotorCommandsTest.py
+++ src/MotorControlsSerial_PWM/MotorCommandsTest.py
@@ -1,13 +1,16 @@
 import TraxxasServoCMD
 import time
 
-TraxxasServoCMD.SteeringCommand(.5)
+TraxxasServoCMD.SteeringCommand(5)
 time.sleep(1)
-TraxxasServoCMD.SteeringCommand(-.5)
+TraxxasServoCMD.SteeringCommand(0.5)
+time.sleep(1)
+TraxxasServoCMD.SteeringCommand(0)
+time.sleep(1)
+TraxxasServoCMD.SteeringCommand(-0.5)
 time.sleep(1)
 TraxxasServoCMD.SteeringCommand(-5)
 time.sleep(1)
-TraxxasServoCMD.SteeringCommand(5)
-time.sleep(1)
 TraxxasServoCMD.SteeringCommand(0)
+
 
