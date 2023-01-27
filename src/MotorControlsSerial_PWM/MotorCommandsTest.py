f065cc839a91eef4b9fcd755b6e58825849ee2ad
GeorgeTMartin <georgetnhmartin@gmail.com>
2023-01-26 17:17:46 -0800

    Got PWM Driver Motor Working

--- src/MotorControlsSerial_PWM/MotorCommandsTest.py
+++ src/MotorControlsSerial_PWM/MotorCommandsTest.py
@@ -1,4 +1,5 @@
 import TraxxasServoCMD
+import TraxxasDriverCMD
 import time
 
 TraxxasServoCMD.SteeringCommand(5)
@@ -12,5 +13,11 @@ time.sleep(1)
 TraxxasServoCMD.SteeringCommand(-5)
 time.sleep(1)
 TraxxasServoCMD.SteeringCommand(0)
+time.sleep(1)
 
-
+TraxxasDriverCMD.ThrottleCommand(0x7FFF)
+time.sleep(6)
+TraxxasDriverCMD.ThrottleCommand(0xFFFF)
+time.sleep(6)
+TraxxasDriverCMD.ThrottleCommand(0x0000)
+time.sleep(6)
