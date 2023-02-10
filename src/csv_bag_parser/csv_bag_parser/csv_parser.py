7609d9434b93e50c0fc662efe5da92a1c07510b4
GeorgeTMartin <georgetnhmartin@gmail.com>
2023-02-09 16:49:19 -0800

    Bumping Raspberry Pi Revision. TODO: Add CSV export.

--- src/csv_bag_parser/csv_bag_parser/csv_parser.py
+++ src/csv_bag_parser/csv_bag_parser/csv_parser.py
@@ -2,21 +2,37 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Imu
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import AccelWithCovarianceStamped
 
 class TopicSubscriberCSV(Node):
 
     def __init__(self):
         super().__init__('csv_parser')
         self.subscription = self.create_subscription(Imu,'imu',self.imu_callback,10)
+        self.subscription = self.create_subscription(AccelWithCovarianceStamped,'accel/filtered',self.filtered_accel_callback,10)
         self.subscription = self.create_subscription(Odometry,'odom',self.odom_callback,10)
+        self.subscription = self.create_subscription(Odometry,'odometry/filtered',self.filtered_odom_callback,10)
         self.subscription  # prevent unused variable warning
 
     def imu_callback(self,msg):
-       self.get_logger().info('I heard: "%f"' % msg.data)
+       x = msg.linear_acceleration.x
+       y = msg.linear_acceleration.y
+       z = msg.linear_acceleration.z
+    #    print('Messages',x,y,z)
 
     def odom_callback(self,msg):
-       self.get_logger().info('I heard: "%f"' % msg.data)
+       v = msg.twist.twist.linear.x
+    #    print('Velocity',v)
 
+    def filtered_accel_callback(self,msg):
+       x_filt = msg.accel.accel.linear.x
+       y_filt = msg.accel.accel.linear.y
+       z_filt = msg.accel.accel.linear.z
+    #    print('Filtered Messages',x_filt,y_filt,z_filt)
+
+    def filtered_odom_callback(self,msg):
+       v_filt = msg.twist.twist.linear.x
+    #    print('Filtered Velocity',v_filt)
 
 
 
