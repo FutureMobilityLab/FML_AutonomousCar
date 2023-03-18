e3abfb28032e897f9933f7ef709d0223d577e681
tvidano <61514829+tvidano@users.noreply.github.com>
2023-03-18 12:11:27 -0700

    Suggested fixes to get youla controller to run.
    Signed-off-by: tvidano <61514829+tvidano@users.noreply.github.com>

--- src/ros2_car_control/ros2_car_control/youlaController.py
+++ src/ros2_car_control/ros2_car_control/youlaController.py
@@ -14,12 +14,33 @@ class YoulaController():
         self.waypoints = waypoints
 
     def get_commands(self,pose_x,pose_y,pose_psi,v):
+        # Not sure if ROS 2 fixes this problem, but you might have to store the
+        # waypoints locally so that the callback doesn't overwrite them.
+        # waypoints = self.waypoints
+
         distance_to_waypoint = []
+        # waypoints = []
 
         for i in range(len(self.waypoints.x)):
             distance_to_waypoint.append((pose_x - self.waypoints.x[i])**2 + (pose_y - self.waypoints.y[i])**2)
+            # waypoints.append([self.waypoints.x[i],self.waypoints.y[i],self.waypoints.psi[i])
             nearest_waypoint_index = distance_to_waypoint.index(min(distance_to_waypoint))
+        # nearest_waypoint_index = np.argmin(distance_to_waypoint)
+        # ref_x, ref_y, ref_yaw = waypoints[nearest_waypoint_index]
+        
+        # lookahead = 0
+        # front_axle_x = pose_x + lookahead * np.cos(pose_psi)
+        # front_axle_y = pose_y + lookahead * np.sin(pose_psi)
 
+        # ref_to_axle = np.array([front_axle_x - ref_x, front_axle_y - ref_y])
+        # crosstrack_vector = np.array([np.sin(ref_yaw), -np.cos(ref_yaw)])
+        # lateral_error = ref_to_axle.dot(crosstrack_vector)
+
+        # This lateral error calculation might not work out if there is a
+        # lookahead distance. Consider the example where there is zero
+        # crosstrack error with a lookahead distance of 1 m. I think this calc
+        # will produce a lateral error of 1 or -1 m. The above calculation is
+        # what I used for the DOT project. Maybe it'll work.
         lateral_error_size = np.sqrt(distance_to_waypoint[nearest_waypoint_index])
         psi_vector = [np.cos(pose_psi),np.sin(pose_psi)]
         error_vector = [self.waypoints.x[nearest_waypoint_index] - pose_x, self.waypoints.y[nearest_waypoint_index]-pose_y]
@@ -27,6 +48,14 @@ class YoulaController():
 
         lateral_error = lateral_error_size  * lateral_error_sign
 
+        # new_Gc_states = self.GcA.dot(self.Gc_states) \
+        #    + self.GcB.dot(lateral_error)
+        # steering_angle = self.GcC.dot(self.Gc_states) \
+        #    + self.GcD.dot(lateral_error)
+
+        # Not sure if you'll have a dimension issue here. If you do try the
+        # code snippet above. Alternatively you replace "*" with "@" in 
+        # python 3.
         new_Gc_states = self.GcA * self.Gc_states + self.GcB * lateral_error
         steering_angle = self.GcC * self.Gc_states + self.GcD * lateral_error
 
