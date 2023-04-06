f8d90e560412c219528a465de0f638aa97cb0afa
GeorgeTMartin <georgetnhmartin@gmail.com>
2023-04-06 09:54:27 -0700

    Made Retuned Code on RPI

--- c_generated_code/build/CMakeFiles/ocp_Dynamic_Bicycle.dir/acados_solver_Dynamic_Bicycle.c.o.d
+++ c_generated_code/build/CMakeFiles/ocp_Dynamic_Bicycle.dir/acados_solver_Dynamic_Bicycle.c.o.d
@@ -1,33 +1,30 @@
 CMakeFiles/ocp_Dynamic_Bicycle.dir/acados_solver_Dynamic_Bicycle.c.o: \
  /home/george/FML_AutonomousCar/c_generated_code/acados_solver_Dynamic_Bicycle.c \
  /usr/include/stdc-predef.h /usr/include/stdio.h \
- /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/aarch64-linux-gnu/bits/libc-header-start.h \
  /usr/include/features.h /usr/include/features-time64.h \
- /usr/include/x86_64-linux-gnu/bits/wordsize.h \
- /usr/include/x86_64-linux-gnu/bits/timesize.h \
- /usr/include/x86_64-linux-gnu/sys/cdefs.h \
- /usr/include/x86_64-linux-gnu/bits/long-double.h \
- /usr/include/x86_64-linux-gnu/gnu/stubs.h \
- /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/stddef.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/stdarg.h \
- /usr/include/x86_64-linux-gnu/bits/types.h \
- /usr/include/x86_64-linux-gnu/bits/typesizes.h \
- /usr/include/x86_64-linux-gnu/bits/time64.h \
- /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
- /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
- /usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h \
- /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
- /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
- /usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h \
- /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
- /usr/include/x86_64-linux-gnu/bits/floatn.h \
- /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
- /usr/include/x86_64-linux-gnu/bits/stdio.h \
- /usr/include/x86_64-linux-gnu/bits/stdio2.h /usr/include/stdlib.h \
- /usr/include/x86_64-linux-gnu/bits/stdlib-bsearch.h \
- /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
- /usr/include/x86_64-linux-gnu/bits/stdlib.h /usr/include/assert.h \
+ /usr/include/aarch64-linux-gnu/bits/wordsize.h \
+ /usr/include/aarch64-linux-gnu/bits/timesize.h \
+ /usr/include/aarch64-linux-gnu/sys/cdefs.h \
+ /usr/include/aarch64-linux-gnu/bits/long-double.h \
+ /usr/include/aarch64-linux-gnu/gnu/stubs.h \
+ /usr/include/aarch64-linux-gnu/gnu/stubs-lp64.h \
+ /usr/lib/gcc/aarch64-linux-gnu/11/include/stddef.h \
+ /usr/lib/gcc/aarch64-linux-gnu/11/include/stdarg.h \
+ /usr/include/aarch64-linux-gnu/bits/types.h \
+ /usr/include/aarch64-linux-gnu/bits/typesizes.h \
+ /usr/include/aarch64-linux-gnu/bits/time64.h \
+ /usr/include/aarch64-linux-gnu/bits/types/__fpos_t.h \
+ /usr/include/aarch64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/include/aarch64-linux-gnu/bits/types/__fpos64_t.h \
+ /usr/include/aarch64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/aarch64-linux-gnu/bits/types/FILE.h \
+ /usr/include/aarch64-linux-gnu/bits/types/struct_FILE.h \
+ /usr/include/aarch64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/aarch64-linux-gnu/bits/floatn.h \
+ /usr/include/aarch64-linux-gnu/bits/floatn-common.h \
+ /usr/include/stdlib.h /usr/include/aarch64-linux-gnu/bits/stdlib-float.h \
+ /usr/include/assert.h \
  /home/george/acados/include/acados_c/ocp_nlp_interface.h \
  /home/george/acados/include/acados/ocp_nlp/ocp_nlp_common.h \
  /home/george/acados/include/acados/ocp_nlp/ocp_nlp_constraints_common.h \
@@ -47,7 +44,7 @@ CMakeFiles/ocp_Dynamic_Bicycle.dir/acados_solver_Dynamic_Bicycle.c.o: \
  /home/george/acados/include/hpipm/include/hpipm_d_ocp_qp_sol.h \
  /home/george/acados/include/hpipm/include/hpipm_d_ocp_qp_sol.h \
  /home/george/acados/include/acados/utils/types.h \
- /usr/lib/gcc/x86_64-linux-gnu/11/include/stdbool.h \
+ /usr/lib/gcc/aarch64-linux-gnu/11/include/stdbool.h \
  /home/george/acados/include/acados/utils/external_function_generic.h \
  /home/george/acados/include/acados/ocp_nlp/ocp_nlp_cost_common.h \
  /home/george/acados/include/acados/ocp_nlp/ocp_nlp_dynamics_common.h \
