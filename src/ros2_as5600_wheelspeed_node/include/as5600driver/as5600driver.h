a207cd95626f055d18981c389849431f5c5d0ef5
tvidano <tvidano@gmail.com>
2023-04-07 00:28:46 -0700

    First draft on FIR filter and add comments.

--- src/ros2_as5600_wheelspeed_node/include/as5600driver/as5600driver.h
+++ src/ros2_as5600_wheelspeed_node/include/as5600driver/as5600driver.h
@@ -4,9 +4,19 @@
 #include "as5600driver/as5600sensor.h"
 #include "rclcpp/rclcpp.hpp"
 #include "nav_msgs/msg/odometry.hpp"
+#include "utils/fir_filter.cpp"
 
+/**
+ * @brief Provides methods for interfacing AS5600 hall-effect sensor 
+ * to ROS2 network.
+ */
 class AS5600Driver : public rclcpp::Node {
  public:
+  /**
+   * @brief Construct a new AS5600Driver instance.
+   * This creates a publisher and publishes the velocity measurement 
+   * at a set frequency.
+   */
   AS5600Driver();
 
  private:
@@ -14,8 +24,22 @@ class AS5600Driver : public rclcpp::Node {
   std::unique_ptr<AS5600Sensor> as5600_;
   size_t count_;
   rclcpp::TimerBase::SharedPtr timer_;
+
+  /**
+   * @brief Publishes the measured velocity from the AS5600 sensor.
+   */
   void handleInput();
+
+  /**
+   * @brief Sets the frequency parameter of the ROS node.
+   */
   void declareParameters();
+
+  // Node Constants.
+  static const std::chrono::duration<int64_t, std::milli> FREQUENCY = 10ms;
+  static const N_TAPS = 1;
+  const double TAP_COEFFS[N_TAPS] = {0.5};
+  FIRFilter fir_filter;
 };
 
 #endif  // AS5600DRIVER_H
--- src/ros2_as5600_wheelspeed_node/src/as5600driver.cpp
+++ src/ros2_as5600_wheelspeed_node/src/as5600driver.cpp
@@ -10,24 +10,33 @@ AS5600Driver::AS5600Driver()
 {
   // Declare parameters
   declareParameters();
+  // Create Filter.
+  fir_filter(N_TAPS, TAP_COEFFS);
   // Create publisher
   publisher_ = this->create_publisher<nav_msgs::msg::Odometry>("odom", 10);
-  std::chrono::duration<int64_t, std::milli> frequency = 10ms;
-  timer_ = this->create_wall_timer(frequency, std::bind(&AS5600Driver::handleInput, this));
+  timer_ = this->create_wall_timer(FREQUENCY, std::bind(&AS5600Driver::handleInput, this));
 }
 
 void AS5600Driver::handleInput()
 {
+  // Initialize an empty odometry message and populate the header.
   auto message = nav_msgs::msg::Odometry();
   message.header.stamp = this->get_clock()->now();
   message.header.frame_id = "base_link";
+  // Compute the velocity from absolute angle measurements.
   message.twist.twist.linear.x = as5600_->getVelocity();
+  if (this->get_parameter("filter")) {
+
+  }
+  // Publish the odometry message where only the linear x velocity
+  // is nonzero.
   publisher_->publish(message);
 }
 
 void AS5600Driver::declareParameters()
 {
   this->declare_parameter<int>("frequency", 0.0);
+  this->declare_parameter<bool>("filter", true);
 }
 
 int main(int argc, char* argv[])
