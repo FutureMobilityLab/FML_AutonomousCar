f854ad426dc0402a20ab6614e73941c8f77f4c41
tvidano <tvidano@gmail.com>
2023-05-12 12:11:18 -0700

    Fix traxxas motor subscription bug.

--- src/ros2_traxxas_controls/ros2_traxxas_controls/ros2_traxxas_motor_commands.py
+++ src/ros2_traxxas_controls/ros2_traxxas_controls/ros2_traxxas_motor_commands.py
@@ -144,7 +144,7 @@ class MotorCommands(Node):
         pca.deinit()
 
     def odom_callback(self, msg):
-        self.v = msg.twist.linear.x
+        self.v = msg.twist.twist.linear.x
 
     def accel_callback(self, msg):
         self.a = msg.linear_acceleration.x
