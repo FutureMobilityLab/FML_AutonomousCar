02cfd401724f97b9e267eb2516a84e563d4c1ab3
tvidano <tvidano@gmail.com>
2023-05-12 09:49:14 -0700

    Reformat rear_wss_ros_node.py and modify to use TwistWithCovariance.

--- src/optical_wss/optical_wss/rear_wss_ros_node.py
+++ src/optical_wss/optical_wss/rear_wss_ros_node.py
@@ -2,26 +2,24 @@
 import rclpy
 from rclpy.node import Node
 import serial
-import time
 
-from geometry_msgs.msg import TwistStamped
+from geometry_msgs.msg import TwistWithCovariance
 
 
 class RearWss(Node):
-    '''Ros2 node for rear optical wheel speed sensors. This connects directly 
-    to an Arduino Uno over a UART protocol. The arduino sends strings that 
+    """Ros2 node for rear optical wheel speed sensors. This connects directly
+    to an Arduino Uno over a UART protocol. The arduino sends strings that
     we have to parse and interpret. It only sends right and left wheel speeds
     and the update rate is inconsistent.
-    '''
+    """
 
     def __init__(self):
-        super().__init__('rear_wss')
+        super().__init__("rear_wss")
         # Setup serial connection with 9600 baud rate and a timeout of 1 sec.
-        self.serial_connection = serial.Serial(
-            '/dev/ttyACM0', 115200, timeout=1)
+        self.serial_connection = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
         # Reset the input buffer to clear incomplete data.
         self.serial_connection.reset_input_buffer()
-        self.msg_buffer = ''
+        self.msg_buffer = ""
 
         self.wheel_radius = 0.057  # [m]
         self.track = 0.28  # [m]
@@ -32,31 +30,31 @@ class RearWss(Node):
         self.rr_wss = 0  # [m/s]
 
         self.twist_publisher = self.create_publisher(
-            TwistStamped, 'rear_wss', 10)
+            TwistWithCovariance, "rear_wss", 10
+        )
 
         # Wait until we receive from the arduino that the connection is complete.
-        msg = ''
-        self.get_logger().info('Waiting for Arduino-Rpi connection to complete.')
-        while msg != 'Setup Complete':
+        msg = ""
+        self.get_logger().info("Waiting for Arduino-Rpi connection to complete.")
+        while msg != "Setup Complete":
             if self.serial_connection.in_waiting > 0:
-                msg = self.serial_connection.readline().decode('utf-8').rstrip()
-        self.get_logger().info('Arduino-Rpi connection complete.')
+                msg = self.serial_connection.readline().decode("utf-8").rstrip()
+        self.get_logger().info("Arduino-Rpi connection complete.")
 
         # Begin sensor loop.
         self.time_of_last_msg = self.get_clock().now().nanoseconds * 1e-9
         self.wss_timer = self.create_timer(0.05, self.get_wss)
 
     def get_wss(self):
-        new_msg = self.serial_connection.read_all().decode('utf-8')
+        new_msg = self.serial_connection.read_all().decode("utf-8")
         # It is possible that the arduino does not send a message. So we handle
         # this with a timeout.
         time_now = self.get_clock().now().nanoseconds * 1e-9
         # The first and last messages may be cutoff, but the middle messages
         # will not be.
         if len(new_msg) < 15:
-            if time_now - self.time_of_last_msg > 1.:
-                self.get_logger().error(
-                    f'time:{time_now - self.time_of_last_msg}')
+            if time_now - self.time_of_last_msg > 1.0:
+                self.get_logger().error(f"time:{time_now - self.time_of_last_msg}")
                 raise IOError
         else:
             self.time_of_last_msg = time_now
@@ -64,8 +62,8 @@ class RearWss(Node):
 
         # Keep only the last 50 chars because they are newest.
         if len(self.msg_buffer) > 50:
-            self.msg_buffer = self.msg_buffer[len(self.msg_buffer) - 50:]
-        msgs = self.msg_buffer.split('\n')
+            self.msg_buffer = self.msg_buffer[len(self.msg_buffer) - 50 :]
+        msgs = self.msg_buffer.split("\n")
 
         # It is assumed the last messages are the most recent.
         for msg in reversed(msgs):
@@ -73,20 +71,20 @@ class RearWss(Node):
             # messages are cutoff. So we filter those out.
             if "RR" not in msg and "RL" not in msg:
                 continue
-            lines = msg.rstrip().split(',')
+            lines = msg.rstrip().split(",")
             # A full message will have 4 lines.
             if len(lines) < 4:
                 continue
             # A full message will not have any empty lines.
-            if not all([len(l) > 0 for l in lines]):
+            if not all([len(line) > 0 for line in lines]):
                 continue
-            self.rl_wss = float(lines[1])*self.wheel_radius
-            self.rr_wss = float(lines[3])*self.wheel_radius
+            self.rl_wss = float(lines[1]) * self.wheel_radius
+            self.rr_wss = float(lines[3]) * self.wheel_radius
             break
 
         # It is not guaranteed that msgs will contain messages. Publish the
         # velocity anyway.
-        twist = TwistStamped()
+        twist = TwistWithCovariance()
         twist.header.stamp = self.get_clock().now().to_msg()
         twist.twist.linear.x = (self.rr_wss + self.rl_wss) / 2
         twist.twist.angular.z = (self.rr_wss - self.rl_wss) / self.track
@@ -106,5 +104,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
