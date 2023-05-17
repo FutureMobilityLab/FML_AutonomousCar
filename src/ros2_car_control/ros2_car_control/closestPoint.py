3c5fe89686eb7919dc5b6362cc0b3b20d6d077cb
tvidano <tvidano@gmail.com>
2023-05-17 12:20:06 -0700

    Add debug prints

--- src/ros2_car_control/ros2_car_control/closestPoint.py
+++ src/ros2_car_control/ros2_car_control/closestPoint.py
@@ -15,7 +15,9 @@ def get_closest_waypoint(
         The waypoint (X,Y,yaw) that is closest the position and its index in waypoints.
     """
 
+    print(f"waypoints shape:{np.shape(waypoints)}")
     dist = np.linalg.norm(pose[0, 0:2] - waypoints[:, 0:2], axis=0)
+    print(f"dist shape:{np.shape(dist)}")
     closest_i = np.argmin(dist)
     return waypoints[closest_i][np.newaxis], closest_i
 
@@ -35,10 +37,13 @@ def get_lateral_errors(
         reference yaw angle.
     """
 
+    print(f"pose:{pose}")
+    print(f"ref_pose:{reference_pose}")
     ref_to_axle = pose[0, 0:2] - reference_pose[0, 0:2]
     crosstrack_vector = np.array(
         [np.sin(reference_pose[0, 2]), -np.cos(reference_pose[0, 2])]
     )
-    crosstrack_err = -ref_to_axle.dot(crosstrack_vector)
+    crosstrack_err = ref_to_axle.dot(crosstrack_vector)
+    print(f"xtrack_err:{crosstrack_err}")
     yaw_err = pose[0, 2] - reference_pose[0, 2]
     return crosstrack_err, yaw_err
