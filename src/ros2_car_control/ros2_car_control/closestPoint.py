Fix closesPoint sign and numpy size issues.

src/ros2_car_control/ros2_car_control/closestPoint.py
@@ -17,7 +17,7 @@ def get_closest_waypoint(
 
     dist = np.linalg.norm(pose[0, 0:2] - waypoints[:, 0:2], axis=0)
     closest_i = np.argmin(dist)
-    return waypoints[closest_i], closest_i
+    return waypoints[closest_i][np.newaxis], closest_i
 
 
 def get_lateral_errors(
@@ -39,6 +39,6 @@ def get_lateral_errors(
     crosstrack_vector = np.array(
         [np.sin(reference_pose[0, 2]), -np.cos(reference_pose[0, 2])]
     )
-    crosstrack_err = ref_to_axle.dot(crosstrack_vector)
+    crosstrack_err = -ref_to_axle.dot(crosstrack_vector)
     yaw_err = pose[0, 2] - reference_pose[0, 2]
     return crosstrack_err, yaw_err

src/ros2_car_control/ros2_car_control/stanleyController.py
@@ -42,7 +42,13 @@ class StanleyController:
         front_axle = np.array(
             [[x + self.L / 2.0 * np.cos(yaw), y + self.L / 2.0 * np.sin(yaw), yaw]]
         )
-        waypoints = np.array([self.waypoints.x, self.waypoints.y, self.waypoints.psi]).T
+        waypoints = np.hstack(
+            (
+                self.waypoints.x[np.newaxis].T,
+                self.waypoints.y[np.newaxis].T,
+                self.waypoints.psi[np.newaxis].T,
+            )
+        )
         closest_waypoint, _ = get_closest_waypoint(front_axle, waypoints)
 
         crosstrack_error, yaw_term = get_lateral_errors(front_axle, closest_waypoint)