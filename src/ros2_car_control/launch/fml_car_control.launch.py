de2e90bdfaad4f24189169304a81803fc75fbbb8
tvidano <61514829+tvidano@users.noreply.github.com>
2023-06-14 12:03:33 -0700

    Integrate bno055 with ekf (#19)
    Major changes:
    - Add BNO055 IMU to EKF configuration.
    - Remove MPU IMU from EKF configuration.
    - Comment out rear IMU in EKF configuration.
    - Increased EKF update rate to 50 Hz.
    
    ---------
    
    Signed-off-by: tvidano <tvidano@gmail.com>
    Co-authored-by: Limited User <futuremobilitylablimiteduser@google.com>

--- src/ros2_car_control/launch/fml_car_control.launch.py
+++ src/ros2_car_control/launch/fml_car_control.launch.py
@@ -6,9 +6,6 @@ import os
 
 def generate_launch_description():
     # For finding yaml config files for optical wss package.
-    wss_pkg_share = launch_ros.substitutions.FindPackageShare(
-        package="optical_wss"
-    ).find("optical_wss")
     ctrl_pkg_share = launch_ros.substitutions.FindPackageShare(
         package="ros2_car_control"
     ).find("ros2_car_control")
@@ -16,12 +13,6 @@ def generate_launch_description():
         package="ros2_traxxas_controls"
     ).find("ros2_traxxas_controls")
     print(trxs_pkg_share)
-    optical_wss_node = launch_ros.actions.Node(
-        package="optical_wss",
-        executable="optical_wss",
-        name="optical_wss",
-        parameters=[{"use_sim_time": LaunchConfiguration("use_sim_time")}],
-    )
 
     traxxas_driver_node = launch_ros.actions.Node(
         package="ros2_traxxas_controls",
@@ -49,35 +40,22 @@ def generate_launch_description():
         name="waypoints",
         output="screen",
     )
-    launch_description = [
-        launch.actions.DeclareLaunchArgument(
-            name="use_sim_time",
-            default_value="False",
-            description="Flag to enable use_sim_time",
-        ),
-        optical_wss_node,
-        traxxas_driver_node,
-        car_controller_node,
-        waypoints_pub_node,
-    ]
-    if False:
-        launch_description.append(
-            launch.actions.ExecuteProcess(
-                cmd=[
-                    "ros2",
-                    "bag",
-                    "record",
-                    "/ref_point",
-                    "/cmd_ackermann",
-                    "/amcl_pose",
-                    "/odometry/filtered",
-                    "/odom",
-                    "/imu",
-                    "/accel/filtered",
-                    "/rear_wss",
-                ],
-                output="screen",
-            )
-        )
 
-    return launch.LaunchDescription(launch_description)
+    return launch.LaunchDescription(
+        [
+            launch.actions.DeclareLaunchArgument(
+                name="use_sim_time",
+                default_value="False",
+                description="Flag to enable use_sim_time",
+            ),
+            # launch.actions.ExecuteProcess(
+            #     cmd=['ros2', 'bag', 'record', '/ref_point', '/cmd_ackermann',
+            #          '/amcl_pose', '/odometry/filtered', '/odom', '/imu',
+            #          '/accel/filtered', '/rear_wss'],
+            #     output='screen'
+            # ),
+            traxxas_driver_node,
+            car_controller_node,
+            waypoints_pub_node,
+        ]
+    )
