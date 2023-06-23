c861b41020da3c2851ce584da86ea3c6d0d35eea
tvidano <61514829+tvidano@users.noreply.github.com>
2023-06-23 13:43:29 -0700

    Improve controller.py readability, simple refactor to use one timer to publish current_pose, point_ref, and ctrl_cmd.

--- src/ros2_car_control/ros2_car_control/controller.py
+++ src/ros2_car_control/ros2_car_control/controller.py
@@ -4,7 +4,7 @@ from rclpy.duration import Duration
 from nav_msgs.msg import Odometry
 from tf_transformations import euler_from_quaternion
 from ackermann_msgs.msg import AckermannDriveStamped
-from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import Pose, PoseStamped
 from std_msgs.msg import String
 from ros2_car_control.stanleyController import StanleyController
 from ros2_car_control.mpcController import MPCController
@@ -14,8 +14,7 @@ from ros2_car_control.fetchWaypoints import waypoints
 from tf2_ros import TransformException
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
-from visualization_msgs.msg import Marker, MarkerArray
-from tf_transformations import quaternion_from_euler
+from visualization_msgs.msg import Marker
 import numpy as np
 
 
@@ -23,56 +22,69 @@ class Controller(Node):
     def __init__(self):
         super().__init__("car_controller")
         FMLCarQoS = QoSProfile(history=1, depth=1, reliability=2, durability=2)
-        self.odometry_subscriber = self.create_subscription(
-            Odometry, "odometry/filtered", self.odometry_callback, FMLCarQoS
-        )
+        ctrl_sample_time = 0.05
+
+        # Create transform listener to get current pose in map frame for position
+        # feedback.
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
         self.odom_frame = ""
         self.map_frame = "map"
+
+        # Subscribe to heartbeat for guaranteeing healthy communication with laptop.
         self.heartbeat_subscriber = self.create_subscription(
             String, "heartbeat", self.heartbeat_callback, FMLCarQoS
         )
+
+        # Subscribe to odometry for current velocity.
+        self.v = 0.0
+        self.odometry_subscriber = self.create_subscription(
+            Odometry, "odometry/filtered", self.odometry_callback, FMLCarQoS
+        )
+
+        # Publish control commands.
         self.ackermann_publisher = self.create_publisher(
             AckermannDriveStamped, "cmd_ackermann", FMLCarQoS
         )
+
+        # Publish the waypoint used as reference for control.
         self.point_ref_publisher = self.create_publisher(Marker, "ref_point", FMLCarQoS)
-        self.pose_hist_publisher = self.create_publisher(
-            MarkerArray, "pose_hist", FMLCarQoS
+
+        # Publish the current pose used as feedback in control (to simplify data
+        # analysis).
+        self.pose_publisher = self.create_publisher(
+            PoseStamped, "current_pose", FMLCarQoS
         )
-        self.waypoints = waypoints()
-        self.cmd_timer = self.create_timer(0.05, self.controller)
-        self.marker_timer = self.create_timer(0.05, self.ref_point)
-        # self.pose_hist_timer = self.create_timer(0.25, self.pose_hist)
-        self.declare_parameter("control_method", "hinf")
-        self.control_method = self.get_parameter("control_method").value
+
+        # Define generic node parameters.
+        self.declare_parameter("control_method", "stanley")
         self.declare_parameter("v_max", 2.0)
-        self.v_max = self.get_parameter("v_max").value
         self.declare_parameter("heartbeat_timeout", 1.0)
-        self.heartbeat_timeout = self.get_parameter("heartbeat_timeout").value
-        self.heartbeat_alarm = 0
-        self.run_flag = 0  # Set to 1 when home testing
-        self.heartbeat_last_time = self.get_clock().now().nanoseconds
-        self.final_thresh = 0.5
-        self.pose_hist_array_x = []
-        self.pose_hist_array_y = []
-        self.pose_hist_array_psi = []
-
-        self.v = 0.0
-        self.x = 0.0
-        self.y = 0.0
-        self.yaw = 0.0
-        self.reference_point_x = 0.0
-        self.reference_point_y = 0.0
-        self.ref_point_marker = Marker()
-        self.pose_markers = MarkerArray()
-
         self.declare_parameter("max_steer", 0.65)
         self.declare_parameter("speed_setpoint", 1.0)
+
+        # Get generic parameter values.
+        self.control_method = self.get_parameter("control_method").value
+        self.v_max = self.get_parameter("v_max").value
+        self.heartbeat_timeout = self.get_parameter("heartbeat_timeout").value
         control_params = {
             "max_steer": self.get_parameter("max_steer").value,
             "speed_setpoint": self.get_parameter("speed_setpoint").value,
         }
+
+        # Define safety-check flags.
+        self.heartbeat_alarm = 0
+        self.run_flag = 0  # Set to 1 when home testing
+        self.heartbeat_last_time = self.get_clock().now().nanoseconds
+        # self.final_thresh = 0.5
+
+        # Get waypoints from json, the specific controller is responsible for finding
+        # and updating the reference points x and y.
+        self.waypoints = waypoints()
+        self.reference_point_x = 0.0
+        self.reference_point_y = 0.0
+
+        # Define and get controller-specific parameters.
         match self.control_method:
             case "pp":
                 self.declare_parameter("pp_lookahead", 0.5)
@@ -99,32 +111,12 @@ class Controller(Node):
                     self.waypoints, control_params
                 )
             case "youla":
-                self.declare_parameter("Youla_GcX", 4)
-                self.declare_parameter(
-                    "Youla_GcA",
-                    [
-                        1.8934,
-                        -1.0907,
-                        0.4767,
-                        -0.2260,
-                        1.0,
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.5,
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.5,
-                        0.0,
-                    ],
-                )
-                self.declare_parameter("Youla_GcB", [8.0, 0.0, 0.0, 0.0])
-                self.declare_parameter("Youla_GcC", [0.5445, -1.2909, 2.0142, -1.0329])
+                self.declare_parameter("Youla_GcX", 1)
+                self.declare_parameter("Youla_GcA", [0.0])
+                self.declare_parameter("Youla_GcB", [0.0])
+                self.declare_parameter("Youla_GcC", [0.0])
                 self.declare_parameter("Youla_GcD", [0.0])
-                self.declare_parameter("Youla_lookahead", 0.5)
+                self.declare_parameter("Youla_lookahead", 0.0)
                 youla_params = {
                     "n_GcX": self.get_parameter("Youla_GcX").value,
                     "GcA": self.get_parameter("Youla_GcA").value,
@@ -136,32 +128,12 @@ class Controller(Node):
                 control_params.update(youla_params)
                 self.controller_function = LTIController(self.waypoints, control_params)
             case "hinf":
-                self.declare_parameter("hinf_n_GcX", 4)
-                self.declare_parameter(
-                    "hinf_GcA",
-                    [
-                        1.8934,
-                        -1.0907,
-                        0.4767,
-                        -0.2260,
-                        1.0,
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.5,
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.0,
-                        0.5,
-                        0.0,
-                    ],
-                )
-                self.declare_parameter("hinf_GcB", [8.0, 0.0, 0.0, 0.0])
-                self.declare_parameter("hinf_GcC", [0.5445, -1.2909, 2.0142, -1.0329])
+                self.declare_parameter("hinf_n_GcX", 1)
+                self.declare_parameter("hinf_GcA", [0.0])
+                self.declare_parameter("hinf_GcB", [0.0])
+                self.declare_parameter("hinf_GcC", [0.0])
                 self.declare_parameter("hinf_GcD", [0.0])
-                self.declare_parameter("hinf_lookahead", 0.5)
+                self.declare_parameter("hinf_lookahead", 0.0)
                 hinf_params = {
                     "n_GcX": self.get_parameter("hinf_n_GcX").value,
                     "GcA": self.get_parameter("hinf_GcA").value,
@@ -186,86 +158,59 @@ class Controller(Node):
                 control_params.update(mpc_params)
                 self.controller_function = MPCController(self.waypoints, control_params)
 
+        # Log controller configuration.
         self.get_logger().info(
             f"INITIALIZING CONTROLLER - TYPE : {self.control_method}"
         )
         for key in control_params:
-            self.get_logger().info(f"{key}: {control_params[key]}")
+            self.get_logger().info(f"\t{key}: {control_params[key]}")
+
+        # Start timers.
+        self.cmd_timer = self.create_timer(ctrl_sample_time, self.controller)
+
+    def heartbeat_callback(self, msg):
+        # When a heartbeat is received, update the time it was received.
+        self.heartbeat_last_time = self.get_clock().now().nanoseconds
 
     def odometry_callback(self, msg):
         self.v = msg.twist.twist.linear.x
-        orientations = msg.pose.pose.orientation
-        quaternion_pose = [
-            orientations.x,
-            orientations.y,
-            orientations.z,
-            orientations.w,
-        ]
-        [_, _, self.yaw] = euler_from_quaternion(quaternion_pose)
-        self.x = msg.pose.pose.position.x
-        self.y = msg.pose.pose.position.y
-        self.odom_frame = msg.header.frame_id
-        self.pose_hist_array_x.append(self.x)
-        self.pose_hist_array_y.append(self.y)
-        self.pose_hist_array_psi.append(self.yaw)
+        # Allow controller to run after first odometry captured.
+        if self.run_flag == 0:
+            self.run_flag = 1
 
-    def heartbeat_callback(self, msg):
-        self.heartbeat_last_time = self.get_clock().now().nanoseconds
+    def publish_ref_point_marker(self):
+        """Publish an RVIZ marker to display the reference point being used."""
+        ref_point_marker = Marker()
+        ref_point_marker.header.frame_id = "map"
+        ref_point_marker.type = Marker.SPHERE
+        ref_point_marker.action = Marker.ADD
+        ref_point_marker.ns = "reference_point"
+        ref_point_marker.id = 1
+        ref_point_marker.pose.position.x = self.reference_point_x
+        ref_point_marker.pose.position.y = self.reference_point_y
+        ref_point_marker.scale.x = 0.1
+        ref_point_marker.scale.y = 0.1
+        ref_point_marker.scale.z = 0.1
+        ref_point_marker.pose.orientation.w = 1.0
+        ref_point_marker.frame_locked = False
+        ref_point_marker.color.a = 1.0
+        ref_point_marker.color.r = 1.0
+        ref_point_marker.lifetime = Duration(seconds=1).to_msg()
 
-    def ref_point(self):
-        self.ref_point_marker.header.frame_id = "map"
-        self.ref_point_marker.type = Marker.SPHERE
-        self.ref_point_marker.action = Marker.ADD
-        self.ref_point_marker.ns = "reference_point"
-        self.ref_point_marker.id = 1
-        self.ref_point_marker.pose.position.x = self.reference_point_x
-        self.ref_point_marker.pose.position.y = self.reference_point_y
-        self.ref_point_marker.scale.x = 0.1
-        self.ref_point_marker.scale.y = 0.1
-        self.ref_point_marker.scale.z = 0.1
-        self.ref_point_marker.pose.orientation.w = 1.0
-        self.ref_point_marker.frame_locked = False
-        self.ref_point_marker.color.a = 1.0
-        self.ref_point_marker.color.r = 1.0
-        self.ref_point_marker.lifetime = Duration(seconds=1).to_msg()
+        self.point_ref_publisher.publish(ref_point_marker)
 
-        self.point_ref_publisher.publish(self.ref_point_marker)
-
-    def pose_hist(self):
-        self.pose_markers.markers = []
-        # now = self.get_clock().now().nanoseconds * 10**-9
-        for i in range(len(self.pose_hist_array_x)):
-            marker_out = Marker()
-            marker_out.header.frame_id = "map"
-            marker_out.header.stamp = self.get_clock().now().to_msg()
-            marker_out.type = Marker.ARROW
-            marker_out.action = Marker.ADD
-            marker_out.ns = "wpts_" + str(i)
-            marker_out.id = i
-            marker_out.pose.position.x = self.pose_hist_array_x[i]
-            marker_out.pose.position.y = self.pose_hist_array_y[i]
-            marker_out.scale.x = 0.1
-            marker_out.scale.y = 0.01
-            marker_out.scale.z = 0.01
-            [ori_x, ori_y, ori_z, ori_w] = quaternion_from_euler(
-                0.0, 0.0, self.pose_hist_array_psi[i]
-            )
-            marker_out.pose.orientation.x = ori_x
-            marker_out.pose.orientation.y = ori_y
-            marker_out.pose.orientation.z = ori_z
-            marker_out.pose.orientation.w = ori_w
-            marker_out.frame_locked = False
-            marker_out.color.a = 1.0
-            marker_out.color.r = 1.0
-            marker_out.lifetime = Duration(seconds=1).to_msg()
-            self.pose_markers.markers.append(marker_out)
-        # duration = self.get_clock().now().nanoseconds * 10**-9
-        # self.get_logger().info(f"Done iterating took:{duration - now}s")
-        self.pose_hist_publisher.publish(self.pose_markers)
-        # now = self.get_clock().now().nanoseconds * 10**-9
-        # self.get_logger().info(f"Done publishing markers took:{now - duration}s")
+    def publish_current_pose(self, pose):
+        """Publish the vehicle's pose."""
+        pose_stamped = PoseStamped()
+        pose_stamped.header.frame_id = "map"
+        pose_stamped.header.stamp = self.get_clock().now().to_msg()
+        pose_stamped.pose = pose
+        self.pose_publisher.publish(self.pose_markers)
 
     def controller(self):
+        """Publish current_pose, ref_point marker, and control command."""
+
+        # Get current pose from transform.
         from_frame = "base_link"
         to_frame = "map"
         try:
@@ -275,35 +220,6 @@ class Controller(Node):
                 f"Could not transform {to_frame} to {from_frame}: {ex}"
             )
             return
-        # if (
-        #     self.get_clock().now().nanoseconds * 10**-9
-        #     - self.heartbeat_last_time * 10**-9
-        #     > self.heartbeat_timeout
-        # ):
-        #     # self.heartbeat_alarm = 1
-        #     self.get_logger().info(f"HEARTBEAT ALARM ACTIVE")
-        #     pass
-
-        # last_waypoint_dist = np.linalg.norm(
-        #     [self.waypoints.x[-1] - self.x, self.waypoints.y[-1] - self.y]
-        # )
-
-        # if (
-        #     self.v > self.v_max
-        #     or self.heartbeat_alarm == 1
-        #     or self.run_flag == 0
-        #     or last_waypoint_dist < self.final_thresh
-        # ):
-        #     # If odometry unsafe, heartbeat fails, run flag disabled, or close
-        #     # enough to end of line
-        #     self.cmd_steer = 0.0
-        #     self.cmd_speed = 0.0
-        #     self.get_logger().info(
-        #         "Odometry unsafe, heartbeat failed, run flag disabled, or close to "
-        #         "end of line."
-        #     )
-        # else:
-        # now = self.get_clock().now().nanoseconds * 10**-9
         quaternion_pose = [
             t.transform.rotation.x,
             t.transform.rotation.y,
@@ -311,32 +227,70 @@ class Controller(Node):
             t.transform.rotation.w,
         ]
         [_, _, yaw] = euler_from_quaternion(quaternion_pose)
+        current_pose = Pose()
+        current_pose.position.x = t.transform.translation.x
+        current_pose.position.y = t.transform.translation.y
+        current_pose.position.z = t.transform.translation.z
+        current_pose.orientation.x = t.transform.rotation.x
+        current_pose.orientation.y = t.transform.rotation.y
+        current_pose.orientation.z = t.transform.rotation.z
+        current_pose.orientation.w = t.transform.rotation.w
+        self.publish_current_pose(current_pose)
+
+        # Publish reference point marker.
+        self.publish_ref_point_marker()
+
+        # Compute control commands.
         (
             self.cmd_steer,
             self.cmd_speed,
             self.reference_point_x,
             self.reference_point_y,
         ) = self.controller_function.get_commands(
-            t.transform.translation.x, t.transform.translation.y, yaw, self.v
+            current_pose.position.x, current_pose.position.y, yaw, self.v
         )
-        # duration = self.get_clock().now().nanoseconds * 10**-9
-        # self.get_logger().info(
-        #     f"Done computing controller cmd took:{duration - now}s"
-        # )
-
         # Clip steer angle to max steer.
         max_steer = self.get_parameter("max_steer").value
         self.cmd_steer = np.clip(self.cmd_steer, -max_steer, max_steer)
+        if self.cmd_steer != max_steer:
+            self.get_logger().warning(
+                f"Steering saturated. Requested {max_steer}, but got {self.cmd_steer}"
+            )
 
-        # self.get_logger().info(f"Steer Command: {self.cmd_steer}")
+        # Raise heartbeat_alarm if heartbeat hasn't been received.
+        if (
+            self.get_clock().now().nanoseconds * 10**-9
+            - self.heartbeat_last_time * 10**-9
+            > self.heartbeat_timeout
+        ):
+            self.heartbeat_alarm = 1
+            self.get_logger().info("HEARTBEAT ALARM ACTIVE")
+            pass
+
+        # last_waypoint_dist = np.linalg.norm(
+        #     [self.waypoints.x[-1] - self.x, self.waypoints.y[-1] - self.y]
+        # )
+
+        if (
+            self.v > self.v_max
+            or self.heartbeat_alarm == 1
+            or self.run_flag == 0
+            # or last_waypoint_dist < self.final_thresh
+        ):
+            # Overwrite commands to steer straight and stop.
+            self.cmd_steer = 0.0
+            self.cmd_speed = 0.0
+            self.get_logger().info(
+                "Speed unsafe, heartbeat failed, run flag disabled, or close to "
+                "end of line."
+            )
+
+        # Publish controller command.
         ackermann_command = AckermannDriveStamped()
         ackermann_command.header.stamp = self.get_clock().now().to_msg()
         ackermann_command.drive.speed = self.cmd_speed
         ackermann_command.drive.steering_angle = self.cmd_steer
-        # now = self.get_clock().now().nanoseconds * 10**-9
         self.ackermann_publisher.publish(ackermann_command)
-        # duration = self.get_clock().now().nanoseconds * 10**-9
-        # self.get_logger().info(f"Done publishing: took:{duration-now}s")
 
 
 def main(args=None):
